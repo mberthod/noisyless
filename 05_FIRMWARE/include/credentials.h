1a95ff7902703ca2b64f555131003252203f60e9
mberthod <86666621+mberthod@users.noreply.github.com>
2025-12-08 11:10:48 +0100

    first commit repertoire

---
+++ 05_FIRMWARE/include/credentials.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Secrets MQTT (placés dans un fichier ignoré par le VCS)
+static const char* MQTT_HOST = "85.215.145.44";
+static const uint16_t MQTT_PORT = 1883;
+static const char* MQTT_USER = "esp32";
+static const char* MQTT_PASS = "am#k^keu5nBDWLB";
+
